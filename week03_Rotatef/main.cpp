58e8a9ff0f9607a1e2e98cf4d609acd7ef947218
ayi1116 <160472828+ayi1116@users.noreply.github.com>
2024-03-04 16:46:31 +0800

    Add files via upload

---
+++ week03_Rotatef/main.cpp
@@ -0,0 +1,35 @@
+#include <GL/glut.h>
+#include <stdio.h>///為了printf()
+float teapotX =0,teapotY =0;///新加的程式用來放茶壺的座標
+float angle = 0;
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);///清背景
+    glPushMatrix();///備份矩陣
+        ///glTranslatef(teapotX,teapotY,0);///把座標移動
+        glRotatef(angle,0,0,1);///最難的Z軸
+        glutSolidTeapot(0.3);
+    glPopMatrix();///還原矩陣
+    glutSwapBuffers();
+}                   ///state:0 :下去 1:上來
+void mouse(int button,int state,int x,int y)
+{///做巧妙的修正
+    teapotX = (x-150/150.0);
+    teapotY = -(y-150/150.0);
+}
+void motion(int x,int y)
+{
+    angle = x;
+    display();
+}
+int main(int argc,char*argv[])
+{
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);
+    glutCreateWindow("week03 mouse");
+
+    glutDisplayFunc(display);
+    glutMouseFunc(mouse);
+    glutMotionFunc(motion);///設定mouse的motion動作
+    glutMainLoop();
+}
